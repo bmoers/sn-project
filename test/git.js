46f5f2168551ddef1fe269e19f198dfdc989af40
Boris Moers <boris@moers.ch>
2018-11-01 08:13:31 +0100

    branch rename merge abort

---
+++ test/git.js
@@ -0,0 +1,9 @@
+const Git = require('../lib/git');
+
+
+const git = new Git({ dir: 'C:\\cicd\\repos\\21c4ee7a-3ed2-4b0a-8a56-8a1837d3b598' })
+
+
+git.branchConfig('va-test-@07cdc464dbd167c0432cfc600f9619e7').then((config) => {
+    console.log(config);
+})

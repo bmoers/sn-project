f21f277100e59eaef1f673f4ecf2a5353da1f797
Boris Moers <boris@moers.ch>
2019-03-25 15:10:12 +0100

    support for 'no-build' text in commit message to not run CICD

--- test/git.js
+++ test/git.js
@@ -1,9 +1,12 @@
 const Git = require('../lib/git');
 
 
-const git = new Git({ dir: 'C:\\cicd\\repos\\21c4ee7a-3ed2-4b0a-8a56-8a1837d3b598' })
+const git = new Git({ dir: 'C:\\cicd\\repos\\21c4ee7a-3ed2-4b0a-8a56-8a1837d3b598---5' })
 
+git.init('no-build');
 
+/*
 git.toBranchName('Virtual Application - APP - 1.1.4@07cdc464dbd167c0432cfc600f9619e7').then((config) => {
     console.log(config);
 })
+*/

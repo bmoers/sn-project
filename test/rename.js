8197c3072baca5c504f68c6c6f9d747e5de75310
Boris Moers <boris@moers.ch>
2019-04-16 14:15:10 +0200

    fix for branch name change

---
+++ test/rename.js
@@ -0,0 +1,12 @@
+const Git = require('../lib/git');
+
+
+const git = new Git({ dir: 'C:\\cicd-server\\repos\\test', quiet: false })
+
+git.switchToBranch('va-atf-collection-@8f392c88db052780432cfc600f9619c7');
+
+/*
+git.toBranchName('Virtual Application - APP - 1.1.4@07cdc464dbd167c0432cfc600f9619e7').then((config) => {
+    console.log(config);
+})
+*/

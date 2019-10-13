2d3df567d30008ea62c3060b8098ba626d57b5b8
Boris Moers <boris@moers.ch>
2019-10-14 00:01:02 +0200

    support for short ssh git protocol fix for git on windows

--- test/init.js
+++ test/init.js
@@ -1,2 +1,11 @@
-var Snproject = require('../index');
-new Snproject();
+const Git = require('../lib/git');
+
+const git = new Git({
+    dir: '/private/tmp/bla3',
+    remoteUrl: 'git@github.com:bmoers/x_11413_cicd_test.git' //  git@github.com:bmoers/x_11413_cicd_test.git
+})
+
+git.init().catch((e) => {
+    console.error(e)
+});
+

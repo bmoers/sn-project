ea17b8369acd14dd01f78a5831e9cea9dc678fde
Boris Moers <boris@moers.ch>
2019-10-16 09:26:40 +0200

    user of homedir

--- test/init.js
+++ test/init.js
@@ -1,7 +1,8 @@
 const Git = require('../lib/git');
 
 const git = new Git({
-    dir: '/private/tmp/bla3',
+    quiet: false,
+    dir: '/private/tmp/test-dir',
     remoteUrl: 'git@github.com:bmoers/x_11413_cicd_test.git' //  git@github.com:bmoers/x_11413_cicd_test.git
 })
 

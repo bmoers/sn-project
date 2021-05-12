ec7cdc4e479a42ab1939db03b8cf7081220e20a2
Boris Moers <boris@moers.ch>
2021-05-12 16:36:50 +0200

    test case for init & push

--- test/init.js
+++ test/init.js
@@ -1,12 +1,39 @@
+const os = require('os');
 const Git = require('../lib/git');
 
-const git = new Git({
-    quiet: false,
-    dir: '/private/tmp/test-dir',
-    remoteUrl: 'git@github.com:bmoers/x_11413_cicd_test.git' //  git@github.com:bmoers/x_11413_cicd_test.git
+let git = new Git({
+    quiet: true,
+    dir: `${os.tmpdir()}/git-branchname-test`,
+    remoteUrl: 'git@github.com:bmoers/x_11413_cicd_test.git'
 });
 
-git.init().catch((e) => {
-    console.error(e);
-});
+(async ()=>{
+
+    console.log('init start');
+    await git.init();
+    console.log('init done');
+
+    await git.switchToBranch('sample_branch_xxxx');
+        
+    await git.add('tmp.file');
+    await git.commit('gula');
+    await git.push('sample_branch_xxxx');
+    
+    //await git.push('sample_branch_3');
+    
+    /*
+    git = new Git({
+        quiet: false,
+        dir: `${os.tmpdir()}/git-branchname-test`,
+        remoteUrl: 'git@github.com:bmoers/x_11413_cicd_test.git'
+    });
+    console.log('git2 init start');
+    await git.init();
+
+    console.log('git2 init done');
+
+    //await git.switchToBranch('sample_branch');
+    await git.push('sample_branch');
+    */
+})();
 
